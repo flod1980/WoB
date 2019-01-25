e8ea8c7eb5ef585a9bf8520b86e9c090f7dfb966
flod1980 <47021759+flod1980@users.noreply.github.com>
2019-01-25 23:18:12 +0100

    Add files via upload

--- src/main/java/Main/OutputFileCreator.java
+++ src/main/java/Main/OutputFileCreator.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
